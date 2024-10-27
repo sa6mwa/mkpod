c96da8368a78d9b5a8b2fadad69c578909c8f370
SA6MWA Michel <sa6mwa@gmail.com>
2024-10-27 23:30:10 +0100

    Add feature to add multiple iTunes categories and release make target

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 NAME = mkpod
 MODULE = github.com/sa6mwa/mkpod
-VERSION = $(shell git describe --tags --abbrev=0 2>/dev/null || echo 0)
+#VERSION = $(shell git describe --tags --abbrev=0 2>/dev/null || echo 0)
+VERSION = v0.1.0
 DESTDIR = /usr/local/bin
 SRC = $(MODULE)/cmd/$(NAME)
 GOOS = $(shell uname -s | tr '[:upper:]' '[:lower:]')
@@ -12,22 +13,33 @@ all: clean build
 
 .PHONY: clean
 clean:
-	rm -f $(NAME)
+	rm -rf bin
 
 .PHONY: build
-build: test $(NAME)
+build: test bin/$(NAME)
 
 .PHONY: test
 test:
 	$(GO) test -cover ./...
 
-$(NAME):
-	$(GO) build -v -ldflags '-s -X main.version=$(VERSION)' -o $(NAME) $(SRC)
+bin:
+	mkdir bin
+
+bin/$(NAME): bin
+	go run golang.org/x/vuln/cmd/govulncheck@latest ./...
+	$(GO) build -v -trimpath -ldflags '-s -w -X main.version=$(VERSION)' -o bin/$(NAME) $(SRC)
+	strip -s bin/$(NAME)
+	if which upx > /dev/null ; then upx -9 bin/$(NAME) ; fi
 
 go.mod:
 	go mod init $(MODULE)
 	go mod tidy
 
 .PHONY: install
-install: $(NAME)
-	install $(NAME) $(DESTDIR)/$(NAME)
+install: bin/$(NAME)
+	install bin/$(NAME) $(DESTDIR)/$(NAME)
+
+.PHONY: release
+release:
+	cp bin/$(NAME) bin/$(NAME)-$(shell go env GOOS)-$(shell go env GOARCH)-$(VERSION)
+	cd bin && sha256sum $(NAME)-$(shell go env GOOS)-$(shell go env GOARCH)-$(VERSION) > checksums.txt
--- cmd/mkpod/types.go
+++ cmd/mkpod/types.go
@@ -362,7 +362,7 @@ type Atom struct {
 	Author        string         `yaml:"author"`
 	Explicit      ItunesExplicit `yaml:"explicit,omitempty"`
 	Keywords      string         `yaml:"keywords"`
-	Category      string         `yaml:"category"`
+	Categories    []Category     `yaml:"categories"`
 	Encoding      struct {
 		Bitrate    int    `yaml:"bitrate"`
 		Lamepath   string `yaml:"lamepath"`
@@ -374,6 +374,42 @@ type Atom struct {
 		Language   string `yaml:"language"`
 	} `yaml:"encoding"`
 	Episodes []Episode `yaml:"episodes"`
+}
+
+type Category struct {
+	Name          string   `yaml:"name"`
+	Subcategories []string `yaml:"subcategories"`
+}
+
+func (c *Category) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var name string
+	if err := unmarshal(&name); err == nil {
+		c.Name = name
+		return nil
+	}
+
+	var mapData map[string][]string
+	if err := unmarshal(&mapData); err == nil {
+		for k, v := range mapData {
+			c.Name = k
+			c.Subcategories = v
+			break
+		}
+		return nil
+	}
+	// Can not use Category type as that makes unmarshalling infinitely recursive
+	type CategoryInternal struct {
+		Name          string   `yaml:"name"`
+		Subcategories []string `yaml:"subcategories"`
+	}
+	var category CategoryInternal
+	if err := unmarshal(&category); err != nil {
+		fmt.Println(err)
+		return err
+	}
+	c.Name = category.Name
+	c.Subcategories = category.Subcategories
+	return nil
 }
 
 type PreProcess struct {
